19526e8890abdee26cb0509d79e2e17090d66c55
lyuzizheng <lyuzizheng@bytedance.com>
2023-05-23 11:51:31 +0800

    ff

--- main.go
+++ main.go
@@ -1,46 +1,11 @@
 package main
 
 import (
-	"code.byted.org/lyuzizheng/playground/sug_reply"
-	"fmt"
+	"code.byted.org/lyuzizheng/playground/jsonx"
 )
 
 func main() {
 
-	sug_reply.SugReply()
+	jsonx.TryJson()
 
-}
-
-type InterfaceA interface {
-	PrintStatus() InterfaceA
-	ChangeStatus() InterfaceA
-}
-
-type A struct {
-	Status string
-}
-
-func (a *A) ChangeStatus() InterfaceA {
-	a.Status = "down"
-	return a
-}
-
-func (a *A) PrintStatus() InterfaceA {
-	fmt.Printf("Status A is %s\n", a.Status)
-	return a
-}
-
-type TestStruct struct {
-	*A
-}
-
-func (t *TestStruct) ChangeStatus() InterfaceA {
-	t.Status = "up"
-	return t
-}
-
-func (t *TestStruct) PrintStatus() InterfaceA {
-	t.A.PrintStatus()
-	//fmt.Printf("Status T is %s\n", t.Status)
-	return t
 }
